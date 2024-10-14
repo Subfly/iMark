25e30fd8b28b9677685ef024a19c29f1706f7ba7
Subfly <alitahasubfly@gmail.com>
2024-10-14 16:36:53 +0200

    Add onboarding view and preload data functionality
    Also:
    - Fix padding issue of create button on home

---
+++ YABA/YABA/FeatureOnboarding/OnboardingGreetingItems.swift
@@ -0,0 +1,63 @@
+//
+// OnboardingGreetingItems.swift
+// YABA
+//
+// Created by Ali Taha on 14.10.2024.
+//
+
+import SwiftUI
+struct OnboardingGreetingItems {
+    private init() {}
+    static func getItems() -> [OnboardingGreetingItem] {
+        return [
+            OnboardingGreetingItem(
+                iconName: "bookmark",
+                label: """
+Easily manage your bookmarks. Create folders and tags. No more hassle of searching your favorite content!
+""",
+                iconBackgroundColorPrimary: .teal,
+                iconBackgroundColorSecondary: .green,
+                appearingOrder: 1
+            ),
+            OnboardingGreetingItem(
+                iconName: "square.and.arrow.up",
+                label: "Share any link from any app, and YABA will take care the rest for you.",
+                iconBackgroundColorPrimary: .indigo,
+                iconBackgroundColorSecondary: .purple,
+                appearingOrder: 2
+            ),
+            OnboardingGreetingItem(
+                iconName: "lock.icloud",
+                label: "Everything is inside your device. No analytics. No data sharing. No tracking.",
+                iconBackgroundColorPrimary: .orange,
+                iconBackgroundColorSecondary: .yellow,
+                appearingOrder: 3
+            ),
+            // TASK: ADD ICLOUD LIMITS FOR THAT SENTENCE
+            OnboardingGreetingItem(
+                iconName: "gift",
+                label: "YABA was, is and will always be free. No ads. No subscriptions. No limits.",
+                iconBackgroundColorPrimary: .blue,
+                iconBackgroundColorSecondary: .mint,
+                appearingOrder: 4
+            ),
+        ]
+    }
+}
+struct OnboardingGreetingItem: Identifiable {
+    let id = UUID()
+    let iconName: String
+    let label: String
+    let iconBackgroundColorPrimary: Color
+    let iconBackgroundColorSecondary: Color
+    let appearingOrder: Int
+    static func empty() -> OnboardingGreetingItem {
+        return OnboardingGreetingItem(
+            iconName: "",
+            label: "",
+            iconBackgroundColorPrimary: .accentColor,
+            iconBackgroundColorSecondary: .accentColor,
+            appearingOrder: 0
+        )
+    }
+}
--- YABA/YABA/FeatureOnboarding/OnboardingView.swift
+++ YABA/YABA/FeatureOnboarding/OnboardingView.swift
@@ -10,11 +10,174 @@ import SwiftUI
 struct OnboardingView: View {
     let onEndOnboarding: () -> Void
     
+    @Environment(\.modelContext)
+    private var modelContext
+
     @State
-    private var currentPage: OnboardingPageType = .greeting
-    
+    private var onboardingVM: OnboardingVM = .init()
+
     var body: some View {
-        Text("Welcome to YABA")
+        VStack {
+            self.titleView
+            Spacer()
+            TabView(selection: self.$onboardingVM.currentPage) {
+                self.greetingPage.tag(OnboardingPageType.greeting)
+                self.finishPage.tag(OnboardingPageType.finish)
+            }.scrollDisabled(true)
+            Spacer()
+            self.button
+        }.padding()
+    }
+
+    @ViewBuilder
+    private var titleView: some View {
+        VStack(spacing: 16) {
+            if self.onboardingVM.currentPage == .greeting {
+                Image("OnboardingAppIcon")
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            } else {
+                Image(systemName: "wand.and.sparkles")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60, alignment: .center)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(
+                                LinearGradient(
+                                    colors: [
+                                        .purple,
+                                        .indigo,
+                                        .purple
+                                    ],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing
+                                )
+                            )
+                            .frame(width: 100, height: 100, alignment: .center)
+                    }
+                    .padding(.bottom)
+            }
+            Text(
+                self.onboardingVM.currentPage == .greeting
+                ? "Welcome to YABA"
+                : "One last thing..."
+            )
+            .font(.largeTitle)
+            .fontWeight(.bold)
+        }
+        .padding(.top, 80)
+        .opacity(self.onboardingVM.shouldShowTitle ? 1 : 0)
+        .onAppear {
+            withAnimation(.smooth.delay(0.75)) {
+                self.onboardingVM.onShowTitle()
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var greetingPage: some View {
+        VStack(alignment: .leading, spacing: 32) {
+            ForEach(OnboardingGreetingItems.getItems()) { item in
+                OnboardingGreetingItemView(item: item)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var finishPage: some View {
+        VStack {
+            Spacer()
+            Text("""
+YABA can create some folders and tags for you, just to give you a headstart. Would you like to do it now?
+"""
+            )
+            .font(.callout)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+            Spacer()
+            Toggle("Create for me", isOn: self.$onboardingVM.createFoldersAndTags)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(Color(UIColor.secondarySystemGroupedBackground))
+                }
+                .padding(.horizontal)
+                .disabled(self.onboardingVM.isCreatingItems)
+            Spacer()
+        }
+    }
+
+    @ViewBuilder
+    private var button: some View {
+        Button {
+            withAnimation {
+                self.onButtonPressed()
+            }
+        } label: {
+            HStack {
+                if self.onboardingVM.isCreatingItems {
+                    ProgressView()
+                } else if self.onboardingVM.itemsCreationDone {
+                    Image(systemName: "checkmark.circle")
+                }
+                Text(
+                    self.onboardingVM.currentPage == .greeting
+                    ? "Continue"
+                    : self.onboardingVM.itemsCreationDone
+                    ? "Start Using YABA"
+                    : self.onboardingVM.isCreatingItems
+                    ? "Creating..."
+                    : self.onboardingVM.createFoldersAndTags
+                    ? "Create"
+                    : "Start Using YABA"
+                )
+            }
+            .fontWeight(.medium)
+            .font(.system(size: 16))
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, maxHeight: 56, alignment: .center)
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(
+                        self.onboardingVM.itemsCreationDone ? .green : .accentColor
+                    )
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .opacity(self.onboardingVM.shouldShowButton ? 1 : 0)
+        .onAppear {
+            withAnimation(.smooth.delay(3.5)) {
+                self.onboardingVM.onShowButton()
+            }
+        }
+        .disabled(self.onboardingVM.isCreatingItems)
+    }
+
+    private func onButtonPressed() {
+        if self.onboardingVM.currentPage == .greeting {
+            self.onboardingVM.onNextStep()
+        } else {
+            if self.onboardingVM.itemsCreationDone {
+                self.onEndOnboarding()
+            } else if self.onboardingVM.createFoldersAndTags {
+                self.onboardingVM.preloadItems { folders, tags in
+                    Task {
+                        folders.forEach { folder in
+                            self.modelContext.insert(folder)
+                        }
+                        tags.forEach { tag in
+                            self.modelContext.insert(tag)
+                        }
+                    }
+                }
+            } else {
+                self.onEndOnboarding()
+            }
+        }
     }
 }
 
